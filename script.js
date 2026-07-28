8d191cb9f90be26ef4d75419cd8f27c0927fceb1
oyinbunughan okpalo <adrielokpalo@gmail.com>
2026-07-28 20:16:54 +0100

    hamburger is now functional

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const heroOrderButton = document.querySelector('.hero-order-btn')
+const hamburger = document.querySelector('.menu-toggle')
+const navList = document.querySelector('.nav-list-items')
 
 
 heroOrderButton.addEventListener('click', function(){
@@ -7,14 +9,7 @@ heroOrderButton.addEventListener('click', function(){
 })
 
 
-function calculateTotal(price1, price2) {
-    return price1 + price2;
-}
-
-const total = calculateTotal(2000, 7000)
-
-function showTotal(total){
-    console.log('your total is ₦' + total)
-}
-
-showTotal(total)
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    navList.classList.toggle('active')
+})
